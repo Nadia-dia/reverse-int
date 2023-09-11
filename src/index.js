48addff8c74c8ea256585c29a2a606b429b80e81
Nadzeya Kaliada <hopenadya88@gmail.com>
2023-09-12 00:15:18 +0200

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let nStr = String(Math.abs(n));
+  let reverseStr = '';
+
+  for(let i = nStr.length - 1; i >= 0; --i) {
+    reverseStr += nStr[i];
+  }
+
+  return Number(reverseStr);
 }
